dbb3e81ccf732319fcbefc7e5276f03a63bf4cf0
JensenKau <jkau0039@student.monash.edu>
2023-04-24 22:56:22 +0800

    update dependencies

--- production/src/main/java/module-info.java
+++ production/src/main/java/module-info.java
@@ -4,9 +4,9 @@ module ninemanmorris {
     requires javafx.fxml;
 
     opens ninemanmorris to javafx.fxml;
-    opens ninemanmorris.Controller to javafx.fxml;
+    opens ninemanmorris.screen.screencontroller to javafx.fxml;
 
     exports ninemanmorris;
-    exports ninemanmorris.Controller;
+    exports ninemanmorris.screen;
     exports ninemanmorris.Enum;
 }
