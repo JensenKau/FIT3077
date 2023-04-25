decb0a88dd167362bfa06c1086348abd0b606f7c
JensenKau <jkau0039@student.monash.edu>
2023-04-25 19:59:50 +0800

    commpleted constructor and added setter method

--- production/src/main/java/ninemanmorris/gamelogic/game/MorrisGame.java
+++ production/src/main/java/ninemanmorris/gamelogic/game/MorrisGame.java
@@ -1,5 +1,7 @@
 package ninemanmorris.gamelogic.game;
 
+import java.util.ArrayList;
+
 import ninemanmorris.gamelogic.IMorrisGameInputHandler;
 import ninemanmorris.gamelogic.IMorrisGameSubscriber;
 import ninemanmorris.gamelogic.MorrisBoard;
@@ -10,19 +12,22 @@ public abstract class MorrisGame implements IMorrisGameInputHandler {
     private MorrisBoard gameBoard;
     private Player[] players;
     private Player currentPlayerTurn;
-    private IMorrisGameSubscriber[] subscribers;
+    private ArrayList<IMorrisGameSubscriber> subscribers;
 
     public MorrisGame(Player p1, Player p2) {
-
+        subscribers = new ArrayList<>();
+        players = new Player[] {p1, p2};
+        currentPlayerTurn = p1;
+        gameBoard = new MorrisBoard();
     }
 
     public void addSubscriber(IMorrisGameSubscriber subscriber) {
-
+        subscribers.add(subscriber);
     }
 
     public void udpateSubscribers() {
         for (IMorrisGameSubscriber subscriber : subscribers) {
-            subscriber.updateGameEnd(false);
+            subscriber.update(currentPlayerTurn.getIsRed(), gameBoard.generatePlayerBoard(currentPlayerTurn.getIsRed()));
         }
     }
 
@@ -36,5 +41,9 @@ public abstract class MorrisGame implements IMorrisGameInputHandler {
 
     protected Player getCurrentPlayerTurn() {
         return currentPlayerTurn;
+    }
+
+    protected void setCurrentPlayerTurn(Player currentPlayerTurn) {
+        this.currentPlayerTurn = currentPlayerTurn;
     }
 }
