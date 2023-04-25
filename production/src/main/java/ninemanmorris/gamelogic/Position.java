f5b3f74996d842114159513b17bb30ab1986f1ef
JensenKau <jkau0039@student.monash.edu>
2023-04-25 16:14:06 +0800

    implemented position and token

--- production/src/main/java/ninemanmorris/gamelogic/Position.java
+++ production/src/main/java/ninemanmorris/gamelogic/Position.java
@@ -2,4 +2,81 @@ package ninemanmorris.gamelogic;
 
 public class Position {
     
+    private Position[] horizontalNeighbours;
+    private Position[] verticalNeighbours;
+    private Token token;
+
+    public Position(Position[] horizontalNeighbours, Position[] verticalNeighbours) {
+        this.horizontalNeighbours = horizontalNeighbours;
+        this.verticalNeighbours = verticalNeighbours;
+    }
+
+    public Token getToken() {
+        return token;
+    }
+
+    public void addToken(Token token) {
+        this.token = token;
+    }
+
+    public Token removeToken() {
+        Token currToken = token;
+
+        token = null;
+
+        return currToken;
+    }
+
+    public Position[] getVerticalNeighbours() {
+        return this.verticalNeighbours;
+    }
+
+    public void setVerticalNeighbours(Position[] positions) {
+        this.verticalNeighbours = positions;
+    }
+
+    public Position[] getHorizontalNeighbours() {
+        return this.horizontalNeighbours;
+    }
+
+    public void setHorizontalNeighbours(Position[] positions) {
+        this.horizontalNeighbours = positions;
+    }
+
+    public Boolean getIsRedToken() {
+        if (token == null) {
+            return null;
+        } 
+        return token.getIsRedPlayer();
+    }
+
+    public boolean getIsMill() {
+        return checkMill(true) || checkMill(false);
+    }
+
+    private boolean checkMill(boolean isVertical) {
+        Position[] neighbours = (isVertical) ? verticalNeighbours : horizontalNeighbours;
+
+        for (int i = 0; i < neighbours.length; i++) {
+            Position[] currentNeighbours = (isVertical) ? neighbours[i].getVerticalNeighbours() : neighbours[i].getHorizontalNeighbours();
+
+            if (currentNeighbours.length == 2) {
+                boolean checkNull = currentNeighbours[0].getToken() != null && neighbours[i] != null && currentNeighbours[1].getToken() != null;
+
+                if (checkNull && currentNeighbours[0].getIsRedToken() == neighbours[i].getIsRedToken() == currentNeighbours[1].getIsRedToken()) {
+                    return true;
+                }
+            }
+        }
+
+        if (neighbours.length == 2) {
+            boolean checkNull = neighbours[0].getToken() != null && token != null && neighbours[1].getToken() != null;
+
+            if (checkNull && neighbours[0].getIsRedToken() == this.getIsRedToken() == neighbours[1].getIsRedToken()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
--- production/src/main/java/ninemanmorris/gamelogic/Token.java
+++ production/src/main/java/ninemanmorris/gamelogic/Token.java
@@ -1,5 +1,20 @@
 package ninemanmorris.gamelogic;
 
+import ninemanmorris.player.playertype.Player;
+
 public class Token {
     
+    private Player player;
+
+    public Token(Player player) {
+        this.player = player;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public boolean getIsRedPlayer() {
+        return player.getIsRed();
+    }
 }
