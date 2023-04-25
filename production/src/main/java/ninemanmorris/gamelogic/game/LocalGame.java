9f749db880b5c0faf847da36f192fe034c9510c5
JensenKau <jkau0039@student.monash.edu>
2023-04-25 14:37:16 +0800

    added method shell for game class

--- production/src/main/java/ninemanmorris/gamelogic/game/LocalGame.java
+++ production/src/main/java/ninemanmorris/gamelogic/game/LocalGame.java
@@ -1,5 +1,15 @@
 package ninemanmorris.gamelogic.game;
 
-public class LocalGame {
-    
+import ninemanmorris.player.playertype.Player;
+
+public class LocalGame extends MorrisGame {
+
+    public LocalGame(Player p1, Player p2) {
+        super(p1, p2);
+    }
+
+    @Override
+    public void handleInput(int x, int y) {
+        
+    }
 }
--- production/src/main/java/ninemanmorris/gamelogic/game/MorrisGame.java
+++ production/src/main/java/ninemanmorris/gamelogic/game/MorrisGame.java
@@ -1,5 +1,40 @@
 package ninemanmorris.gamelogic.game;
 
-public class MorrisGame {
+import ninemanmorris.gamelogic.IMorrisGameInputHandler;
+import ninemanmorris.gamelogic.IMorrisGameSubscriber;
+import ninemanmorris.gamelogic.MorrisBoard;
+import ninemanmorris.player.playertype.Player;
+
+public abstract class MorrisGame implements IMorrisGameInputHandler {
     
+    private MorrisBoard gameBoard;
+    private Player[] players;
+    private Player currentPlayerTurn;
+    private IMorrisGameSubscriber[] subscribers;
+
+    public MorrisGame(Player p1, Player p2) {
+
+    }
+
+    public void addSubscriber(IMorrisGameSubscriber subscriber) {
+
+    }
+
+    public void udpateSubscribers() {
+        for (IMorrisGameSubscriber subscriber : subscribers) {
+            subscriber.updateGameEnd(false);
+        }
+    }
+
+    protected MorrisBoard getGameBoard() {
+        return gameBoard;
+    }
+
+    protected Player[] getPlayers() {
+        return players;
+    }
+
+    protected Player getCurrentPlayerTurn() {
+        return currentPlayerTurn;
+    }
 }
