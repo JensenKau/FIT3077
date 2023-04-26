9929b6da973889de31e698f0d06bc556f478c4fd
JensenKau <jkau0039@student.monash.edu>
2023-04-26 11:39:10 +0800

    implemented random move in computer ai

--- production/src/main/java/ninemanmorris/player/playertype/ComputerAI.java
+++ production/src/main/java/ninemanmorris/player/playertype/ComputerAI.java
@@ -2,9 +2,15 @@ package ninemanmorris.player.playertype;
 
 import ninemanmorris.move.Move;
 
+import java.util.Random;
+
 public class ComputerAI extends Player {
 
     private static final boolean IS_REQUIRE_INPUT = false;
+    private static final Random RAND_INSTANCE = new Random();
+
+    private static final int BOARD_LENGTH = 7;
+    private static final int BOARD_WIDTH = 7;
 
     public ComputerAI(boolean isRed) {
         super(isRed);
@@ -19,8 +25,7 @@ public class ComputerAI extends Player {
     public Move getMove() {
         Move currentMove = super.getMove();
 
-        // TODO: Need to generate random values here
-        currentMove.setMovePosition(0, 0);
+        currentMove.setMovePosition(RAND_INSTANCE.nextInt(BOARD_LENGTH), RAND_INSTANCE.nextInt(BOARD_WIDTH));
 
         return currentMove;
     }
